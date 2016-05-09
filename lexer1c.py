db59d80dadc7b9e37da3754eb909e485d097dc96
rmusiy <rmusiy@gmail.com>
2016-05-09 23:14:56 +0300

    added new lexems and some grammar rules

--- lexer1c.py
+++ lexer1c.py
@@ -1,33 +1,47 @@
 # ------------------------------------------------------------
 # lexer1c.py
 #
-# tokenizer for a 1C:Eterprise embedded language "1C"
+# tokenizer for a 1C:Enterprise embedded language "1C"
 # ------------------------------------------------------------
 import ply.lex as lex
+import re
 
 # reserved words
-
 reserved = {
     '#если'         : 'DEF_IF',
     '#тогда'        : 'DEF_THEN',
+    '#иначеесли'    : 'DEF_ELSE_IF',
     '#иначе'        : 'DEF_ELSE',
     '#конецесли'    : 'DEF_ENDIF',
+    '#область'      : 'AREA',
+    '#конецобласти' : 'AREA_END',
     'если'          : 'IF',
     'тогда'         : 'THEN',
     'иначе'         : 'ELSE',
     'конецесли'     : 'ENDIF',
     'для'           : 'FOR',
     'по'            : 'TO',
+    'из'            : 'FROM',
     'пока'          : 'WHILE',
     'цикл'          : 'DO',
-    'конеццикла'    : 'ENDDO',
+    'конеццикла'    : 'END_DO',
+    'продолжить'    : 'CONTINUE',
+    'прервать'      : 'BREAK',
     'функция'       : 'FUNCTION',
-    'конецфункции'  : 'ENDFUNCTION',
+    'конецфункции'  : 'END_FUNCTION',
     'процедура'     : 'PROCEDURE',
-    'конецпроцедуры': 'ENDPROCEDURE',
+    'конецпроцедуры': 'END_PROCEDURE',
+    'перейти'       : 'GOTO',
     'неопределено'  : 'UNDEFINED',
-    'перем'         : 'PEREM',
-    'экспорт'       : 'EXPORT'
+    'знач'          : 'VAL',
+    'перем'         : 'VAR',
+    'экспорт'       : 'EXPORT',
+    'истина'        : 'TRUE',
+    'ложь'          : 'FALSE',
+    'не'            : 'NOT',
+    'и'             : 'AND',
+    'или'           : 'OR',
+    'новый'         : 'NEW'
 }
 
 # List of token names. This is always required.
@@ -44,19 +58,23 @@ tokens = [
    'TIMES',     # *
    'DIVIDE',    # /
    'COMMA',     # ,
-   'SEMICOLON', # ;
+   'SEMI',      # ;
    'DOT',       # .
    'LPAREN',    # (
    'RPAREN',    # )
+   'COLON',     # :
 
    'ID', 
+   'FOR_EACH',
    'PREPROCID',
    'COMMENT',
-   'DIRECTIVE'] + list(reserved.values())
+   'DIRECTIVE',
+   'LABEL'] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_STRING    = r'"(?:[^"]|"")*"'
 t_DIRECTIVE = r'&[a-zA-Zа-яА-Я]*'
+t_LABEL     = r'~[a-zA-Zа-яА-Я_][a-zA-Zа-яА-Я_0-9]*'
 t_LSB       = r'\['
 t_RSB       = r'\]'
 t_AMRSND    = r'\&'
@@ -66,16 +84,28 @@ t_MINUS     = r'-'
 t_TIMES     = r'\*'
 t_DIVIDE    = r'/'
 t_COMMA     = r','
-t_SEMICOLON = r';'
+t_SEMI      = r';'
 t_DOT       = r'\.'
 t_LPAREN    = r'\('
 t_RPAREN    = r'\)'
-t_COMMENT   = r'\/\/.*\n'
+t_COLON     = r':'
+
+# comment
+def t_COMMENT(t):
+    r'\/\/.*\n'
+    pass # just pass comment
 
 # preprocessor instructions
 def t_PREPROCID(t):
     r'\#[a-zA-Zа-яА-Я_][a-zA-Zа-яА-Я_0-9]*'
     t.type = reserved.get(t.value.lower(), 'unknown preprocessor instruction: ' + t.value)
+    return t
+
+# double identificator
+def t_FOR_EACH(t):
+    r'для[\s]*каждого'
+    t.type  = 'FOR_EACH'
+    t.value = 'Для Каждого'
     return t
 
 # identificator
@@ -104,10 +134,10 @@ def t_error(t):
     t.lexer.skip(1)
 
 # Build the lexer
-lexer = lex.lex()
+lexer = lex.lex(reflags=re.I)
 
 if __name__ == '__main__':
-    data = open("samples/sample.1c", encoding='utf-8').read()
+    data = open("samples/sample_1.1c", encoding='utf-8').read()
     lexer.input(data)
     for lextoken in lexer:
         print(lextoken)
--- parser1c.py
+++ parser1c.py
@@ -3,12 +3,14 @@ import ply.yacc as yacc
 # Get the token map from the lexer.  This is required.
 from lexer1c import tokens
 
+
 ## function & procedure declaration
 
 def p_func_declaration_1(p):
-    '''function_decl : function_head func_body ENDFUNCTION
-                     | function_head ENDFUNCTION'''
+    '''function_decl : function_head func_body END_FUNCTION
+                     | function_head END_FUNCTION'''
     pass
+
 
 def p_func_head_1(p):
     '''function_head : DIRECTIVE FUNCTION ID params_decl
@@ -17,37 +19,46 @@ def p_func_head_1(p):
                      | FUNCTION ID params_decl EXPORT'''
     pass
 
+
 def p_func_params_1(p):
     '''params_decl : LPAREN RPAREN'''
     pass
+
 
 def p_params_decl(p):
     '''params_decl : LPAREN declarator_list RPAREN'''
     pass
 
+
 def p_init_declarator_list_1(t):
     '''declarator_list : init_declarator'''
     pass
+
 
 def p_init_declarator_list_2(t):
     '''declarator_list : declarator_list COMMA init_declarator'''
     pass
 
+
 def p_init_declarator_1(t):
     '''init_declarator : declarator'''
     pass
+
 
 def p_init_declarator_2(t):
     '''init_declarator : declarator EQ initializer'''
     pass
 
+
 def p_declarator_1(t):
-    '''declarator : znach param_name'''
+    '''declarator : VAL param_name'''
     pass
+
 
 def p_declarator_2(t):
     '''declarator : param_name'''
     pass
+
 
 def p_initializer(t):
     '''initializer : STRING
@@ -56,52 +67,105 @@ def p_initializer(t):
     '''
     pass
 
+
 def p_param_name(p):
     '''param_name : ID'''
     pass
 
-def p_znach_modifier(p):
-    '''znach : ID'''
-    if not p[1].lower() == 'знач':
-        raise Exception("Неверный модификатор ЗНАЧ: " + str(p[1]))
-    pass
 
 def p_func_body_1(p):
     '''func_body : perem_decl_list statements_list'''
     pass
+
 
 def p_func_body_2(p):
     '''func_body : perem_decl_list
                  | statements_list'''
     pass
 
+
 def p_perem_decl_list_1(p):
     '''perem_decl_list : perem_decl_list perem_decl'''
     pass
+
 
 def p_perem_decl_list_2(p):
     '''perem_decl_list : perem_decl'''
     pass
 
+
 def p_perem_decl(p):
-    '''perem_decl : PEREM ID SEMICOLON'''
+    '''perem_decl : VAR ID SEMI'''
     pass
+
 
 def p_statements_list_1(p):
-    '''statements_list : statements_list statement'''
+    '''statements_list : statements_list SEMI'''
     pass
+
 
 def p_statements_list_2(p):
-    '''statements_list : statement'''
+    '''statements_list : SEMI'''
     pass
 
-def p_statement(p):
-    '''statement : ID SEMICOLON'''
-    pass
+## -- BEGIN EXPRESSION --
 
-def p_statement_1(p):
-    '''statement : ID SEMICOLON'''
-    pass
+# def p_expression_plus(p):
+#     'expression : expression PLUS term'
+#     pass
+#
+#
+# def p_expression_minus(p):
+#     'expression : expression MINUS term'
+#     pass
+#
+#
+# def p_term_times(p):
+#     'term : term TIMES factor'
+#     pass
+#
+#
+# def p_term_div(p):
+#     'term : term DIVIDE factor'
+#     pass
+#
+#
+# def p_factor_num(p):
+#     '''operand : NUMBER'''
+#     pass
+#
+#
+# def p_factor_expr(p):
+#     'factor : LPAREN expression RPAREN'
+#     pass
+
+
+## -- END EXPRESSION --
+
+
+# def p_statement_1(p):
+#     '''statement : preproc_block
+#                  | area_block
+#                  | for_each_block
+#                  | for_block
+#                  | while_block
+#                  | if_block
+#                  | SEMI
+#                  '''
+#     pass
+#
+# def p_statement_2(p):
+#     '''statement : lvalue = rvalue SEMI'''
+#     pass
+#
+# def p_statement_3(p):
+#     '''statement : rvalue SEMI'''
+#     pass
+#
+# def p_lvalue(p):
+#     ''''''
+#     pass
+
 
 # Error rule for syntax errors
 def p_error(p):
