000bb0a1f5f877e063f37ba321cb307dc1bf72a3
rmusiy <rmusiy@gmail.com>
2016-05-18 14:17:47 +0300

    Updated allowed complecated statements

--- parser1c.py
+++ parser1c.py
@@ -2,9 +2,12 @@ import ply.yacc as yacc
 from lexer1c import tokens
 
 precedence = (
-               ('left', 'PLUS','MINUS'),
-               ('left', 'TIMES','DIVIDE'),
-               ('left', 'MOD')
+               ('left', 'EQ', 'NOT_EQ', 'LT', 'LE', 'GT', 'GE'),
+               ('left', 'OR', 'AND'),
+               ('left', 'PLUS', 'MINUS'),
+               ('left', 'TIMES', 'DIVIDE', 'MOD'),
+               ('right', 'UNOT'),
+               ('right', 'UMINUS', 'UPLUS')
 )
 
 ## -- begin function & procedure declaration --
@@ -34,7 +37,7 @@ def p_init_declarator_list_2(t):
     pass
 
 def p_init_declarator_list_3(t):
-    '''declarator_list : '''
+    '''declarator_list : empty'''
     pass
 
 def p_init_declarator_1(t):
@@ -71,16 +74,12 @@ def p_bool(p):
 ## -- begin function & procedure body --
 
 def p_func_body_1(p):
-    '''func_body : vars_decls_list semi statements_list'''
+    '''func_body : vars_decls_list semi statements'''
     pass
 
 def p_func_body_2(p):
-    '''func_body : vars_decls_list semi statements_list semi'''
-    pass
-def p_func_body_3(p):
     '''func_body : vars_decls_list
-                 | vars_decls_list semi
-                 | statements_list'''
+                 | statements'''
     pass
 
 def p_perem_decl_list_1(p):
@@ -98,7 +97,19 @@ def p_perems_list_1(p):
 def p_perems_list_2(p):
     '''vars_list : ID'''
 
-## statements_list
+## statements
+
+def p_statements_1(p):
+    '''statements : statements_list semi'''
+    pass
+
+def p_statements_2(p):
+    '''statements : statements_list'''
+    pass
+
+def p_statements_3(p):
+    '''statements : empty'''
+    pass
 
 def p_statements_list_1(p):
     '''statements_list : statements_list semi statement'''
@@ -110,8 +121,21 @@ def p_statements_list_2(p):
 
 ## -- end function & procedure body --
 
+def p_statement_1(p):
+    '''statement : prop_complex EQ expr'''
+    pass
+
 def p_statement_2(p):
-    '''statement : variable EQ NUMBER'''
+    '''statement : func_call'''
+    pass
+
+def p_statement_3(p):
+    '''statement : CONTINUE
+                 | BREAK'''
+    pass
+
+def p_statement_4(p):
+    '''statement : GOTO LABEL'''
     pass
 
 def p_semi_1(p):
@@ -122,111 +146,208 @@ def p_semi_2(p):
     '''semi : SEMI'''
     pass
 
-## -- BEGIN VARIABLE --
-## TODO
+## -- BEGIN statement  --
 
-def p_var_1(p):
-    '''variable : ID'''
+def p_prop_complex_1(p):
+    '''prop_complex : prop_complex DOT property'''
     pass
 
-def p_var_2(p):
-    '''variable : ID square_brackets_list'''
+def p_prop_complex_2(p):
+    '''prop_complex : func_call DOT property'''
     pass
 
-def p_square_brackets_list_1(p):
-   '''square_brackets_list : square_brackets_list LSB expr RSB'''
-   pass
+def p_prop_complex_3(p):
+    '''prop_complex : property'''
+    pass
 
-def p_square_brackets_list_2(p):
-    '''square_brackets_list : LSB expr RSB'''
+def p_property_1(p):
+    '''property : property LSB expr RSB'''
+    pass
+
+def p_property_2(p):
+    '''property : func_call LSB expr RSB'''
+    pass
+
+def p_property_3(p):
+    '''property : ID'''
     pass
 
 def p_expr_simple(p):
-    '''expr : bool
+    '''expr : prop_complex
+            | func_call
+            | bool
             | NUMBER
+            | DATE
             | STRING
-            | UNDEFINED
-            | ID'''
+            | UNDEFINED'''
     pass
 
-# def p_properties_list_1(p):
-#    '''properties_list : properties_list DOT variable'''
-#    pass
-#
-# def p_properties_list_2(p):
-#    '''properties_list : DOT variable'''
-#    pass
+def p_expr_binary(p):
+    '''expr : expr PLUS expr
+            | expr MINUS expr
+            | expr TIMES expr
+            | expr DIVIDE expr
+            | expr MOD expr
+            | expr OR expr
+            | expr AND expr
+            | expr NOT_EQ expr
+            | expr LT expr
+            | expr LE expr
+            | expr GT expr
+            | expr GE expr
+            | expr EQ expr
+            '''
+    # | expr EQ expr
+    pass
 
-## -- END VARIABLE --
+def p_expr_group(p):
+    '''expr : LPAREN expr RPAREN'''
+    pass
 
-## -- BEGIN EXPRESSION --
+def p_expr_not(p):
+    'expr : NOT expr %prec UNOT'
+    pass
 
-# def p_expr_binary(p):
-#     '''expr : expr PLUS expr
-#             | expr MINUS expr
-#             | expr TIMES expr
-#             | expr DIVIDE expr
-#             | expr MOD expr'''
-#     pass
+def p_expr_uminus(p):
+    'expr : MINUS expr %prec UMINUS'
+    pass
 
-# def p_expr_simple(p):
-#     '''expr : bool
-#             | NUMBER
-#             | STRING
-#             | UNDEFINED
-#             | ID'''
-#             #| func_call'''
-#     pass
+def p_expr_uplus(p):
+    'expr : PLUS expr %prec UPLUS'
+    pass
 
-# def p_expr_group(p):
-#     '''expr : LPAREN expr RPAREN'''
-#     pass
-#
-# ## -- END EXPRESSION --
-#
-# def p_func_call(p):
-#     '''func_call : ID LPAREN params_list RPAREN'''
-#     pass
-#
-# def p_params_list_1(p):
-#     '''params_list : '''
-#     pass
-#
-# def p_params_list_2(p):
-#     '''params_list : expr'''
-#     pass
-#
-# def p_params_list_3(p):
-#     '''params_list : params_list COMMA expr'''
-#     pass
+def p_func_call(p):
+    '''func_call : ID LPAREN params_list RPAREN'''
+    pass
 
+def p_params_list_1(p):
+    '''params_list : empty'''
+    pass
 
+def p_params_list_2(p):
+    '''params_list : expr'''
+    pass
 
-#
-# # def p_statement_1(p):
-# #     '''statement : preproc_block
-# #                  | area_block
-# #                  | for_each_block
-# #                  | for_block
-# #                  | while_block
-# #                  | if_block
-# #                  | SEMI
-# #                  '''
-# #     pass
-# #
-# # def p_statement_2(p):
-# #     '''statement : lvalue = rvalue SEMI'''
-# #     pass
-# #
-# # def p_statement_3(p):
-# #     '''statement : rvalue SEMI'''
-# #     pass
-# #
-# # def p_lvalue(p):
-# #     ''''''
-# #     pass
-#
-#
+def p_params_list_3(p):
+    '''params_list : params_list COMMA expr'''
+    pass
+
+## -- END statement --
+
+## -- BEGIN complex statement --
+
+def p_statement_complex_1(p):
+    '''statement : for_each_block
+                 | for_block
+                 | while_block
+                 | if_else_block
+                 | try_block
+                 | preproc_if_else_block'''
+    pass
+
+def p_for_each(p):
+    '''for_each_block : FOR_EACH ID FROM expr DO statements END_DO'''
+    pass
+
+def p_for_block(p):
+    '''for_block : FOR ID EQ expr TO expr DO statements END_DO'''
+    pass
+
+def p_while_block(p):
+    '''while_block : WHILE expr DO statements END_DO'''
+    pass
+
+def p_try_block(p):
+    '''try_block : TRY statements EXCEPTION statements END_TRY'''
+    pass
+
+## -- BEGIN if-elseif-else-endif block --
+
+def p_if_else_block(p):
+    '''if_else_block : IF expr THEN statements else_block END_IF'''
+    pass
+
+def p_else_block_empty(p):
+    '''else_block : empty'''
+    pass
+
+def p_else_block_1(p):
+    '''else_block : else_if_collection ELSE statements'''
+    pass
+
+def p_else_block_2(p):
+    '''else_block : else_if_collection'''
+    pass
+
+def p_else_block_3(p):
+    '''else_block : ELSE statements'''
+    pass
+
+def p_else_if_collection_1(p):
+    '''else_if_collection : else_if_collection else_if'''
+    pass
+
+def p_else_if_collection_2(p):
+    '''else_if_collection : else_if'''
+    pass
+
+def p_else_if_block(p):
+    '''else_if : ELSE_IF expr THEN statements'''
+    pass
+
+## -- END if-elseif-else-endif block --
+
+def p_preproc_if_else_block_1(p):
+    '''preproc_if_else_block : DEF_IF preproc_expr THEN statements DEF_END_IF'''
+    pass
+
+def p_preproc_if_else_block_2(p):
+    '''preproc_if_else_block : DEF_IF preproc_expr THEN statements preproc_else_block DEF_END_IF'''
+    pass
+
+def p_preproc_else_block_1(p):
+    '''preproc_else_block : preproc_else_if_collection DEF_ELSE statements'''
+    pass
+
+def p_preproc_else_block_2(p):
+    '''preproc_else_block : preproc_else_if_collection'''
+    pass
+
+def p_preproc_else_block_3(p):
+    '''preproc_else_block : DEF_ELSE statements'''
+    pass
+
+def p_preproc_else_if_collection_1(p):
+    '''preproc_else_if_collection : preproc_else_if_collection preproc_else_if'''
+    pass
+
+def p_preproc_else_if_collection_2(p):
+    '''preproc_else_if_collection : preproc_else_if'''
+    pass
+
+def p_preproc_else_if_block(p):
+    '''preproc_else_if : DEF_ELSE_IF preproc_expr THEN statements'''
+    pass
+
+def p_preproc_expr_1(p):
+    '''preproc_expr : ID'''
+    pass
+
+def p_preproc_expr_2(p):
+    '''preproc_expr : preproc_expr AND preproc_expr
+                    | preproc_expr OR preproc_expr'''
+    pass
+
+def p_preproc_expr_3(p):
+    '''preproc_expr : LPAREN preproc_expr RPAREN'''
+    pass
+
+## -- END complex statement --
+
+def p_empty(p):
+    '''empty : '''
+    pass
+
 # Error rule for syntax errors
 def p_error(p):
     print("Syntax error in input!")
