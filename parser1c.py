6d152042717876a7d0cd8f1067967623f39a706d
rmusiy <rmusiy@gmail.com>
2016-05-29 21:34:05 +0300

    Complete code structure processing

--- parser1c.py
+++ parser1c.py
@@ -33,17 +33,16 @@ def p_module_4(p):
 def p_global_vars_1(p):
     '''global_vars : global_vars SEMI directive VAR global_var_decl_list'''
     p[0] = [x for x in p[1]]
-    p[0].append(strct1c.VariablesDeclaration(p[3], p[5]))
+    p[0].append(strct1c.VariablesDeclaration(p[5], p[3]))
 
 def p_global_vars_2(p):
     '''global_vars : directive VAR global_var_decl_list'''
-    p[0] = [strct1c.VariablesDeclaration(p[1], p[3])]
+    p[0] = [strct1c.VariablesDeclaration(p[3], p[1])]
 
 def p_global_var_decl_list_1(p):
     '''global_var_decl_list : global_var_decl_list COMMA global_var_decl'''
     p[0] = [x for x in p[1]]
     p[0].append(p[3])
-    pass
 
 def p_global_var_decl_list_2(p):
     '''global_var_decl_list : global_var_decl'''
@@ -63,89 +62,75 @@ def p_directive(p):
 
 def p_directive_empty(p):
     '''directive : empty'''
-    p[0] = None
+    p[0] = p[1]
 
 ## funcs & procs
 
-def p_func_decls_1(p):
-    '''proc_func_list : proc_func_list func_decl'''
-    pass
+def p_func_decls(p):
+    '''proc_func_list : proc_func_list func_decl
+                      | proc_func_list proc_decl'''
+    p[0] = [x for x in p[1]]
+    p[0].append(p[2])
 
-def p_func_decls_2(p):
-    '''proc_func_list : proc_func_list proc_decl'''
-    pass
-
-def p_func_decls_3(p):
+def p_func_decls_one(p):
     '''proc_func_list : func_decl
                       | proc_decl'''
-    pass
+    p[0] = [p[1]]
 
 ## -- begin function & procedure declaration --
 
-def p_func_decl_1(p):
-    '''func_decl : func_begin ID LPAREN declarator_list RPAREN func_body END_FUNCTION'''
-    pass
+def p_func_decl(p):
+    '''func_decl : directive FUNCTION ID LPAREN declarator_list RPAREN func_body END_FUNCTION'''
+    p[0] = strct1c.Function(True, False, p[1], p[3], p[5], p[7])
 
-def p_func_decl_2(p):
-    '''func_decl : func_begin ID LPAREN declarator_list RPAREN EXPORT func_body END_FUNCTION'''
-    pass
+def p_func_decl_export(p):
+    '''func_decl : directive FUNCTION ID LPAREN declarator_list RPAREN EXPORT func_body END_FUNCTION'''
+    p[0] = strct1c.Function(True, True, p[1], p[3], p[5], p[8])
 
-def p_proc_decl_1(p):
-    '''proc_decl : proc_begin ID LPAREN declarator_list RPAREN func_body END_PROCEDURE'''
-    pass
+def p_proc_decl(p):
+    '''proc_decl : directive PROCEDURE ID LPAREN declarator_list RPAREN func_body END_PROCEDURE'''
+    p[0] = strct1c.Function(False, False, p[1], p[3], p[5], p[7])
 
-def p_proc_decl_2(p):
-    '''proc_decl : proc_begin ID LPAREN declarator_list RPAREN EXPORT func_body END_PROCEDURE'''
-    pass
+def p_proc_decl_export(p):
+    '''proc_decl : directive PROCEDURE ID LPAREN declarator_list RPAREN EXPORT func_body END_PROCEDURE'''
+    p[0] = strct1c.Function(False, True, p[1], p[3], p[5], p[8])
 
-def p_func_begin(p):
-    '''func_begin : directive FUNCTION'''
-    pass
-
-def p_proc_begin(p):
-    '''proc_begin : directive PROCEDURE'''
-    pass
-
-def p_init_declarator_list_1(t):
+def p_init_declarator_list_1(p):
     '''declarator_list : declarator_list COMMA init_declarator'''
-    pass
+    p[0] = [x for x in p[1]]
+    p[0].append(p[3])
 
-def p_init_declarator_list_2(t):
+def p_init_declarator_list_2(p):
     '''declarator_list : init_declarator'''
-    pass
+    p[0] = [p[1]]
 
-def p_init_declarator_list_3(t):
+def p_init_declarator_list_3(p):
     '''declarator_list : empty'''
-    pass
+    p[0] = strct1c.Empty
 
-def p_init_declarator_1(t):
+def p_init_declarator_1(p):
     '''init_declarator : ID'''
-    pass
+    p[0] = strct1c.FuncVarInitDecl(False, p[1])
 
-def p_init_declarator_2(t):
+def p_init_declarator_2(p):
     '''init_declarator : VAL ID'''
-    pass
+    p[0] = strct1c.FuncVarInitDecl(True, p[1])
 
-def p_init_declarator_3(t):
+def p_init_declarator_3(p):
     '''init_declarator : ID EQ func_param_initializer'''
-    pass
+    p[0] = strct1c.FuncVarInitDecl(False, p[1], p[3])
 
-def p_init_declarator_4(t):
+def p_init_declarator_4(p):
     '''init_declarator : VAL ID EQ func_param_initializer'''
-    pass
+    p[0] = strct1c.FuncVarInitDecl(True, p[2], p[4])
 
-def p_initializer(t):
+def p_initializer(p):
     '''func_param_initializer : bool
                               | strings
-                              | NUMBER
-                              | DATE
-                              | UNDEFINED'''
-    pass
-
-def p_bool(p):
-    '''bool : TRUE
-            | FALSE'''
-    pass
+                              | number
+                              | date
+                              | undefined'''
+    p[0]=p[1]
 
 ## -- end function & procedure declaration --
 
@@ -153,27 +138,33 @@ def p_bool(p):
 
 def p_func_body_1(p):
     '''func_body : vars_decls_list SEMI statements'''
-    pass
+    p[0] = strct1c.FuncBody(vars_decls_list=p[1], statements=p[3])
 
-def p_func_body_2(p):
-    '''func_body : vars_decls_list
-                 | statements'''
-    pass
+def p_func_body_vars_decls_list(p):
+    '''func_body : vars_decls_list'''
+    p[0] = strct1c.FuncBody(vars_decls_list=p[1])
+
+def p_func_body_statements(p):
+    '''func_body : statements'''
+    p[0] = strct1c.FuncBody(statements=p[1])
 
 def p_perem_decl_list_1(p):
     '''vars_decls_list : vars_decls_list SEMI VAR vars_list'''
-    pass
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.VariablesDeclaration(p[4]))
 
 def p_perem_decl_list_2(p):
     '''vars_decls_list : VAR vars_list'''
-    pass
-
+    p[0] = [strct1c.VariablesDeclaration(p[2])]
 
 def p_perems_list_1(p):
     '''vars_list : vars_list COMMA ID'''
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.VarDesc(p[3]))
 
 def p_perems_list_2(p):
     '''vars_list : ID'''
+    p[0] = [strct1c.VarDesc(p[1])]
 
 ## statements
 
@@ -189,77 +180,73 @@ def p_statements_simple(p):
 def p_statements_error(p):
     '''statements : statements error SEMI statement'''
     print ("Incorect statement on line %d" % p.lexer.lineno)
-    pass
 
 ## -- end function & procedure body --
 
-def p_statement(p):
+def p_statement_block(p):
     '''statement : iteration_statement
                  | jump_statement
                  | if_else_statement
                  | try_statement
                  | labeled_statement
                  | preproc_statement'''
-    pass
+    p[0] = p[1]
 
 def p_statement_eq(p):
     '''statement : property EQ expr'''
-    p[0] = strct1c.StatementSimple(p[1], p[3])
+    p[0] = strct1c.StatementAssignment(p[1], p[3])
 
 def p_statement_func_call(p):
     '''statement : property'''
-    # здесь вместо property должен быть сложный вызов func_call,
-    # например для выражения a.a[1].f()
-    p[0] = strct1c.StatementSimple(p[1])
-    pass
+    # todo добавить проверку, что оканчивается на вызов функции
+    # todo например, p.p.p[1].f() - вырено, а p.p.p[1] - нет
+    p[0] = strct1c.StatementFuncCall(p[1])
 
 def p_statement_empty(p):
     '''statement : empty'''
-    pass
+    p[0] = strct1c.Empty
 
 def p_try_statement(p):
     '''try_statement : TRY statements EXCEPTION statements END_TRY'''
-    pass
+    p[0] = strct1c.TryStatement(p[2], p[4])
 
 ## labeled statement
 
 def p_labeled_statement(p):
     '''labeled_statement : LABEL COLON statement'''
-    pass
+    p[0] = strct1c.LabeledStatement(p[1], p[3])
 
 ## if-else statement
 
 def p_if_else_statement(p):
     '''if_else_statement : IF expr THEN statements else_block END_IF'''
-    pass
+    p[0] = strct1c.IfElseStatement(p[2], p[4], p[5])
 
 def p_else_block_empty(p):
     '''else_block : empty'''
-    pass
+    p[0] = strct1c.Empty()
 
 def p_else_block_1(p):
     '''else_block : else_if_collection ELSE statements'''
-    pass
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.ElseCollection(p[3]))
 
 def p_else_block_2(p):
     '''else_block : else_if_collection'''
-    pass
+    p[0] = p[1]
 
 def p_else_block_3(p):
     '''else_block : ELSE statements'''
-    pass
+    p[0] = [strct1c.ElseCollection(p[2])]
 
 def p_else_if_collection_1(p):
-    '''else_if_collection : else_if_collection else_if'''
-    pass
+    '''else_if_collection : else_if_collection ELSE_IF expr THEN statements'''
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.ElseCollection(p[5], p[3]))
 
 def p_else_if_collection_2(p):
-    '''else_if_collection : else_if'''
-    pass
-
-def p_else_if_block(p):
-    '''else_if : ELSE_IF expr THEN statements'''
-    pass
+    '''else_if_collection : ELSE_IF expr THEN statements'''
+    p[0] = [strct1c.ElseCollection(p[4], p[2])]
 
 ## iteration statements
 
@@ -267,87 +254,89 @@ def p_iteration_statement(p):
     '''iteration_statement : for_each_block
                            | for_block
                            | while_block'''
-    pass
+    p[0] = p[1]
 
 def p_for_each(p):
     '''for_each_block : FOR_EACH ID FROM expr DO statements END_DO'''
-    pass
+    p[0] = strct1c.ForEachBlock(p[2], p[4], p[6])
 
 def p_for_block(p):
     '''for_block : FOR ID EQ expr TO expr DO statements END_DO'''
-    pass
+    p[0] = strct1c.ForBlock(p[2], p[4], p[6], p[8])
 
 def p_while_block(p):
     '''while_block : WHILE expr DO statements END_DO'''
-    pass
+    p[0] = strct1c.WhileBlock(p[2], p[4])
 
 ## jump statements
 def p_jump_statement(p):
+    '''jump_statement : CONTINUE
+                      | BREAK'''
+    p[0] = strct1c.JumpStatemets(p[1])
+
+def p_jump_statement_two(p):
     '''jump_statement : RETURN expr_opt
                       | RAISE expr_opt
-                      | CONTINUE
-                      | BREAK
                       | GOTO LABEL'''
-    pass
+    p[0] = strct1c.JumpStatemets(p[1], p[2])
 
 def p_expr_opt_1(t):
     '''expr_opt : empty'''
-    pass
+    p[0] = p[1]
 
 def p_expr_opt_2(t):
     '''expr_opt : expr'''
-    pass
+    p[0] = p[1]
 
 ## preproc statement
 
-def p_preproc_if_else_block_1(p):
-    '''preproc_statement : DEF_IF preproc_expr THEN statements DEF_END_IF'''
-    pass
-
-def p_preproc_if_else_block_2(p):
+def p_preproc_if_else_block(p):
     '''preproc_statement : DEF_IF preproc_expr THEN statements preproc_else_block DEF_END_IF'''
-    pass
+    p[0] = strct1c.PreprocIfElseStatement(p[2], p[4], p[5])
+
+def p_preproc_else_block_empty(p):
+    '''preproc_else_block : empty'''
+    p[0] = strct1c.Empty()
 
 def p_preproc_else_block_1(p):
     '''preproc_else_block : preproc_else_if_collection DEF_ELSE statements'''
-    pass
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.PreprocElseCollection(p[3]))
 
 def p_preproc_else_block_2(p):
     '''preproc_else_block : preproc_else_if_collection'''
-    pass
+    p[0] = [x for x in p[1]]
 
 def p_preproc_else_block_3(p):
     '''preproc_else_block : DEF_ELSE statements'''
-    pass
+    p[0] = [strct1c.PreprocElseCollection(p[2])]
 
 def p_preproc_else_if_collection_1(p):
-    '''preproc_else_if_collection : preproc_else_if_collection preproc_else_if'''
-    pass
+    '''preproc_else_if_collection : preproc_else_if_collection DEF_ELSE_IF preproc_expr THEN statements'''
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.PreprocElseCollection(p[5], p[3]))
 
 def p_preproc_else_if_collection_2(p):
-    '''preproc_else_if_collection : preproc_else_if'''
-    pass
-
-def p_preproc_else_if_block(p):
-    '''preproc_else_if : DEF_ELSE_IF preproc_expr THEN statements'''
-    pass
-
-def p_preproc_expr_1(p):
-    '''preproc_expr : ID'''
-    pass
-
-def p_preproc_expr_not(p):
-    '''preproc_expr : NOT preproc_expr %prec UNOT'''
-    pass
+    '''preproc_else_if_collection : DEF_ELSE_IF preproc_expr THEN statements'''
+    p[0] = [strct1c.PreprocElseCollection(p[4], p[2])]
 
 def p_preproc_expr_2(p):
     '''preproc_expr : preproc_expr AND preproc_expr
                     | preproc_expr OR preproc_expr'''
-    pass
+    p[0] = strct1c.PreprocExprBinary(p[1], p[3], p[2])
 
-def p_preproc_expr_3(p):
+def p_preproc_expr_rb(p):
     '''preproc_expr : LPAREN preproc_expr RPAREN'''
-    pass
+    p[0] = strct1c.PreprocExpr(p[1], True)
+
+def p_preproc_expr_simple(p):
+    '''preproc_expr : ID'''
+    p[0] = strct1c.PreprocExpr(p[1], False)
+
+def p_preproc_expr_not(p):
+    '''preproc_expr : NOT preproc_expr %prec UNOT'''
+    p[0] = strct1c.PreprocExprNot(p[2])
+
 
 ## -- BEGIN statement  --
 
@@ -380,10 +369,10 @@ def p_func_call(p):
 def p_expr_simple(p):
     '''expr : property
             | bool
-            | NUMBER
-            | DATE
+            | number
+            | date
             | strings
-            | UNDEFINED'''
+            | undefined'''
     p[0] = strct1c.SimpleExpr(p[1])
 
 def p_expr_group(p):
@@ -409,39 +398,40 @@ def p_expr_binary(p):
 
 def p_expr_qstn(p):
     '''expr : QSTN LPAREN expr COMMA expr COMMA expr RPAREN'''
-    pass
+    p[0] = strct1c.QuestionExpr(p[3], p[5], p[7])
 
 def p_expr_new_1(p):
     '''expr : NEW ID'''
-    pass
+    p[0] = strct1c.NewExpr(id=p[2])
 
 def p_expr_new_2(p):
     '''expr : NEW ID LPAREN params_list RPAREN'''
-    pass
+    p[0] = strct1c.NewExpr(id=p[2], param_list=p[4])
 
 def p_expr_new_3(p):
     '''expr : NEW LPAREN new_params_list RPAREN'''
-    pass
+    p[0] = strct1c.NewExpr(param_list=p[3])
 
 def p_new_params_list_1(p):
     '''new_params_list : new_params_list COMMA expr'''
-    pass
+    p[0] = [x for x in p[1]]
+    p[0].append(p[3])
 
 def p_new_params_list_2(p):
     '''new_params_list : expr'''
-    pass
+    p[0] = [p[1]]
 
 def p_expr_not(p):
     '''expr : NOT expr %prec UNOT'''
-    pass
+    p[0] = strct1c.UnaryExpr(p[1], p[2])
 
 def p_expr_uminus(p):
     '''expr : MINUS expr %prec UMINUS'''
-    pass
+    p[0] = strct1c.UnaryExpr(p[1], p[2])
 
 def p_expr_uplus(p):
     '''expr : PLUS expr %prec UPLUS'''
-    pass
+    p[0] = strct1c.UnaryExpr(p[1], p[2])
 
 def p_params_list_1(p):
     '''params_list : params_list COMMA expr'''
@@ -461,13 +451,31 @@ def p_params_list_expr(p):
 
 ## -- END statement --
 
-def p_strings_1(p):
-    '''strings : strings STRING'''
-    p[0] = strct1c.StringsSeq(p[1], p[2])
+def p_bool(p):
+    '''bool : TRUE
+            | FALSE'''
+    p[0] = strct1c.Bool(p[1])
 
-def p_strings_2(p):
+def p_number(p):
+    '''number : NUMBER'''
+    p[0] = strct1c.Number(p[1])
+
+def p_date(p):
+    '''date : DATE'''
+    p[0] = strct1c.Date(p[1])
+
+def p_undefined(p):
+    '''undefined : UNDEFINED'''
+    p[0] = strct1c.Undefined()
+
+def p_strings_list(p):
+    '''strings : strings STRING'''
+    p[0] = [x for x in p[1]]
+    p[0].append(strct1c.Strings(p[2]))
+
+def p_strings(p):
     '''strings : STRING'''
-    p[0] = strct1c.StringsBase(p[1])
+    p[0] = [strct1c.Strings(p[1])]
 
 def p_empty(p):
     '''empty : '''
@@ -483,3 +491,4 @@ parser = yacc.yacc()
 if __name__ == '__main__':
     data = open("samples/sample.1c", encoding='utf-8').read()
     result = parser.parse(data)
+    pass
--- strct1c.py
+++ strct1c.py
@@ -11,9 +11,9 @@ class Module:
 class VariablesDeclaration:
     directive = None
     vars_list = None
-    def __init__(self, directive, vars_list):
-        self.directive = directive
+    def __init__(self, vars_list, directive = None):
         self.vars_list = vars_list
+        self.directive = directive
 
 class VarDesc:
     name = None
@@ -22,16 +22,117 @@ class VarDesc:
         self.name = name
         self.export = export
 
+class Function:
+    is_function = None
+    is_export = None
+    directive = None
+    name = None
+    vars_list = None
+    body = None
+    def __init__(self, is_function, is_export, directive, name, vars_list, body):
+        self.is_function = is_function
+        self.is_export = is_export
+        self.directive = directive
+        self.name = name
+        self.vars_list = vars_list
+        self.body = body
+
+class FuncBody:
+    vars_decls_list = None
+    statements = None
+    def __init__(self, vars_decls_list = None, statements = None):
+        self.vars_decls_list = vars_decls_list
+        self.statements = statements
+
+class FuncVarInitDecl:
+    is_val = None
+    var_name = None
+    init_value = None
+    def __init__(self, is_val, var_name, init_value = None):
+        self.is_val = is_val
+        self.var_name = var_name
+        self.init_value = init_value
+
 class Statement:
     pass
 
-class StatementSimple(Statement):
+class StatementAssignment(Statement):
     # for expression like 'a = 1 + 2' and 'f()'
     property = None
     expr = None
     def __init__(self, property, expr = None):
         self.property = property
         self.expr = expr
+
+class StatementFuncCall(Statement):
+    func_call_path = None
+    def __init__(self, func_call_path):
+        self.func_call_path = func_call_path
+
+class TryStatement(Statement):
+    try_statements = None
+    except_statements = None
+    def __init__(self, try_statements, except_statements):
+        self.try_statements = try_statements
+        self.except_statements = except_statements
+
+class LabeledStatement(Statement):
+    label_name = None
+    statement = None
+    def __init__(self, label_name, statement):
+        self.label_name = label_name
+        self.statement = statement
+
+class IfElseStatement(Statement):
+    if_expression = None
+    if_statements = None
+    else_collection = None
+    def __init__(self, if_expression, if_statements, else_collection):
+        self.if_expression = if_expression
+        self.if_statements = if_statements
+        self.else_collection = else_collection
+
+class ElseCollection:
+    condition = None
+    statements = None
+    def __init__(self, statements, condition = None):
+        self.condition = condition
+        self.statements = statements
+
+class ForEachBlock(Statement):
+    id_name = None
+    expression = None
+    statements = None
+    def __init__(self, id_name, expression, statements):
+        self.id_name = id_name
+        self.expression = expression
+        self.statements = statements
+
+class ForBlock(Statement):
+    id_name = None
+    expression_start = None
+    expression_end = None
+    statements = None
+    def __init__(self, id_name, expression_start, expression_end, statements):
+        self.id_name = id_name
+        self.expression_start = expression_start
+        self.expression_end = expression_end
+        self.statements = statements
+
+class WhileBlock(Statement):
+    expression = None
+    statements = None
+    def __init__(self, expression, statements):
+        self.expression = expression
+        self.statements = statements
+
+class JumpStatemets(Statement):
+    key_word = None
+    second_param = None
+    def __init__(self, key_word, second_param):
+        self.key_word = key_word
+        self.second_param = second_param
+
 
 class Property:
     property_left = None
@@ -58,67 +159,127 @@ class FuncCall:
 class ParamList:
     pass
 
-
 class ParamListEmpty(ParamList):
     pass
 
-
 class ParamListExpr(ParamList):
-
     expr = None
-
     def __init__(self, expr):
         self.expr = expr
 
-
 class ParamListComma(ParamList):
-
     param_left = None
     param_right = None
-
     def __init__(self, param_left, param_right = None):
         self.param_left = param_left
         self.param_right = param_right
-
 
 class Expr:
     pass
 
 class SimpleExpr(Expr):
-
     expr = None
     is_group = None
-
     def __init__(self, expr, is_group=False):
         self.expr = expr
         self.is_group = is_group
 
 class BinaryExpr(Expr):
-
     expr1 = None
     expr2 = None
     oper  = None
-
     def __init__(self, expr1, expr2, oper):
         self.expr1 = expr1
         self.expr2 = expr2
         self.oper = oper
 
-class Strings:
-    pass
+class QuestionExpr(Expr):
+    expr = None
+    first = None
+    second = None
+    def __init__(self, expr, first, second):
+        self.expr = expr
+        self.first = first
+        self.second = second
 
-class StringsBase(Strings):
+class NewExpr(Expr):
+    id = None
+    param_list = None
+    def __init__(self, id = None, param_list = None):
+        self.id = id
+        self.param_list = param_list
+
+class UnaryExpr(Expr):
+    op = None
+    expr = None
+    def __init__(self, op, expr):
+        self.op = op
+        self.expr = expr
+
+## PREPROC
+
+class PreprocIfElseStatement:
+    if_expression = None
+    if_statements = None
+    else_collection = None
+    def __init__(self, if_expression, if_statements, else_collection):
+        self.if_expression = if_expression
+        self.if_statements = if_statements
+        self.else_collection = else_collection
+
+
+class PreprocElseCollection:
+    condition = None
+    statements = None
+    def __init__(self, statements, condition = None):
+        self.condition = condition
+        self.statements = statements
+
+class PreprocExpr:
+    expr = None
+    brackets = None
+    def __init__(self, expr, brackets):
+        self.expr = expr
+        self.brackets = brackets
+
+class PreprocExprBinary:
+    expr_left = None
+    expr_right = None
+    oper = None
+    def __init__(self, expr_left, expr_right, oper):
+        self.expr_left = expr_left
+        self.expr_right = expr_right
+        self.oper = oper
+
+class PreprocExprNot:
+    expr = None
+    def __init__(self, expr):
+        self.expr = expr
+
+## SIMPLE TYPES
+
+class Bool:
     value = None
     def __init__(self, value):
         self.value = value
 
+class Strings:
+    value = None
+    def __init__(self, value):
+        self.value = value
 
-class StringsSeq(Strings):
-    strings_left  = None
-    strings_right = None
-    def __init__(self, strings_left, strings_right):
-        self.strings_left = strings_left
-        self.strings_right = strings_right
+class Number:
+    value = None
+    def __init__(self, value):
+        self.value = value
+
+class Date:
+    value = None
+    def __init__(self, value):
+        self.value = value
+
+class Undefined:
+    pass
 
 class Empty:
     pass
